d4ef19daacf975e890a1157437f69e9177707d87
ferney <Ferneybatistagbf@gmail.com>
2021-04-30 15:36:50 -0500

    Commit 2: final R

--- Conculta-2.sql
+++ Conculta-2.sql
@@ -1,9 +1,8 @@
-select idusuario, 
+select Us.idusuario, 
 		CONCAT(Us.nombres, ' ', Us.apellidos) as "Nombre Completo",
 		Us.email as "Email",
-		RTxU as "Reproducciones totales",
-		DATE_PART('day', InfRexUsu.UFR::timestamp - InfRexUsu.LFR::timestamp),
-		(InfRexUsu.UFR-InfRexUsu.LFR)
+		InfRexUsu.RTxU as "Reproducciones totales",
+		(InfRexUsu.UFR-InfRexUsu.LFR)/31 as "meses activo"
 from usuarios US inner join (select idusuario, 
 							 count(RE.idreproducción) AS RTxU,  
 							 Max(RE.fechareproducción) AS UFR, 
