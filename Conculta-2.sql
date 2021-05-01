98874a42e8460787d0181efa9e3a7d24bad824eb
ferney <Ferneybatistagbf@gmail.com>
2021-04-30 21:35:51 -0500

    Cimit 3/posible final

--- Conculta-2.sql
+++ Conculta-2.sql
@@ -1,11 +1,17 @@
-select Us.idusuario, 
+select DISTINCT Us.idusuario, 
 		CONCAT(Us.nombres, ' ', Us.apellidos) as "Nombre Completo",
 		Us.email as "Email",
 		InfRexUsu.RTxU as "Reproducciones totales",
-		(InfRexUsu.UFR-InfRexUsu.LFR)/31 as "meses activo"
-from usuarios US inner join (select idusuario, 
-							 count(RE.idreproducción) AS RTxU,  
-							 Max(RE.fechareproducción) AS UFR, 
-							 min(RE.fechareproducción)  as LFR 
+		CONCAT((100*(count(CASE WHEN DiasR BETWEEN 1 AND 5 Then idreproducción END)))/InfRexUsu.RTxU,'%') as "Porcentage de reproduccion durante semana",
+		CONCAT((100*(count(CASE WHEN DiasR BETWEEN 6 AND 7 Then idreproducción END)))/InfRexUsu.RTxU,'%') as "Porcentage de reproduccion durante fin de semana",
+		(InfRexUsu.UFR-InfRexUsu.LFR)/31 as "meses activo",
+		(SELECT sum(montopagado) from pagos P where P.idusuario=Us.idusuario) AS "Total Pagado en planes"
+from usuarios US inner join (select idreproducción,
+							 idusuario, 
+							 count(RE.idreproducción)  over(PARTITION BY idusuario) AS RTxU,  
+							 Max(RE.fechareproducción) over(PARTITION BY idusuario) AS UFR, 
+							 min(RE.fechareproducción) over(PARTITION BY idusuario) as LFR,
+							 EXTRACT(ISODOW FROM fechaReproducción) as DiasR
 							 from reproducciones RE 
-							 GROUP BY idusuario) as InfRexUsu using(idusuario)
+							 WHERE segundosReproducidos>=60) as InfRexUsu using(idusuario)
+GROUP BY Us.idusuario,InfRexUsu.RTxU,InfRexUsu.UFR,InfRexUsu.LFR
--- probar.sql
+++ probar.sql
@@ -1,1 +1,12 @@
-Select * FROM canciones
+SELECT DISTINCT idusuario,idplan, sum(montopagado) over(PARTITION BY idusuario,idplan) as PagadoxP
+from pagos
+GROUP by idusuario,idpago
+
+SELECT idusuario,count(CASE WHEN DiasR BETWEEN 1 AND 5 Then idreproducción END) "durante semana",
+count(CASE WHEN DiasR BETWEEN 6 AND 7 Then idreproducción END) "fin de"
+from (select idreproducción,idusuario, 
+      EXTRACT(ISODOW FROM fechaReproducción) as DiasR
+	  from reproducciones RE ) as InfRexUsu
+GROUP BY idusuario
+
+SELECT C.título, I.nombre, ROUND((SELECT AVG(calificación) FROM Calificaciones WHERE Calificaciones.idCanción = C.idCanción), 1), SUM(cantidadReproducciones), CONCAT(ROUND(100.0 * SUM(cantidadReproducciones)/ (SELECT COUNT(idReproducción) FROM Reproducciones INNER JOIN Canciones USING(idCanción) WHERE Canciones.idintérpreteprincipal = C.idintérpreteprincipal ), 1), '%'), CONCAT(SUM(CASE WHEN díaDeLaSemana BETWEEN 1 AND 5 THEN cantidadReproducciones END), ' (', ROUND(100.0 * SUM(CASE WHEN díaDeLaSemana BETWEEN 1 AND 5 THEN cantidadReproducciones END)/SUM(cantidadReproducciones), 1), '%)') AS "reproducciones de lunes a viernes", CONCAT(SUM(CASE WHEN díaDeLaSemana BETWEEN 6 AND 7 THEN cantidadReproducciones END), ' (', ROUND(100.0 * SUM(CASE WHEN díaDeLaSemana BETWEEN 6 AND 7 THEN cantidadReproducciones END)/SUM(cantidadReproducciones), 1), '%)') AS "reproducciones de sábado y domingo" FROM Intérpretes I INNER JOIN Canciones C ON I.idIntérprete = C.idintérpreteprincipal INNER JOIN (SELECT idCanción, EXTRACT(ISODOW FROM fechaReproducción) AS díaDeLaSemana, COUNT(idReproducción) AS cantidadReproducciones FROM Reproducciones WHERE segundosReproducidos>=30 GROUP BY idCanción, EXTRACT(ISODOW FROM fechaReproducción)) AS R USING (idCanción) GROUP BY I.idIntérprete, C.idCanción ORDER BY SUM(cantidadReproducciones) DESC
